df81a0f35aab2d25b7a6c20cb17c2edc53677934
OsamaAzhar101 <osamaazhar591@gmail.com>
2025-07-13 18:21:08 +0400

    Add PaymentResponse model and implement payment details retrieval by order ID

--- src/main/java/com/oasys/PaymentService/service/PaymentService.java
+++ src/main/java/com/oasys/PaymentService/service/PaymentService.java
@@ -1,8 +1,11 @@
 package com.oasys.PaymentService.service;
 
 import com.oasys.PaymentService.model.PaymentRequest;
+import com.oasys.PaymentService.model.PaymentResponse;
 
 public interface PaymentService {
 
     long processPaymentInitiate(PaymentRequest paymentRequest);
+
+    PaymentResponse  getPaymentDetailsByOrderId(long orderId);
 }
